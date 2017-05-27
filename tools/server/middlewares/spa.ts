46e74619743c46c14bd8f768b34764281b0d61ec
Peter Yun <pyun@tidemark.com>
2017-05-27 17:42:59 +0900

    feat(env): add service-uri.json for restful api when check service or static on SPA of stubs env.

--- tools/server/middlewares/spa.ts
+++ tools/server/middlewares/spa.ts
@@ -2,9 +2,12 @@ import * as path from 'path';
 import * as url from 'url';
 import * as fs from 'fs-extra';
 import * as glob from 'glob';
+import * as util from 'gulp-util';
+
 import Config from '../../config';
 
 let staticUrls = require(path.join(process.cwd(), 'tools/config/stub_api/static-uri.json'));
+let serviceUrls = require(path.join(process.cwd(), 'tools/config/stub_api/service-uri.json'));
 
 module.exports = function (options: any) {
     if (!options) {
@@ -17,31 +20,46 @@ module.exports = function (options: any) {
     return function (req: any, res: any, next: any) {
         const pathname = url.parse(req.url).pathname;
         let isStatic: boolean = false;
+        let isService: boolean = false;
         let isIndex: boolean = false;
         if (pathname === '/') {
             isStatic = true;
             isIndex = true;
         } else {
-            staticUrls.forEach((url: any) => {
-                if (isStatic) { return; }
-                isStatic = pathname.indexOf(url.path) === 0;
-                if (isStatic) {
-                    isIndex = url.index;
-                    return;
+            if (pathname) {
+                // For Restful service call
+                serviceUrls.forEach((url: any) => {
+                    if (isService) { return; }
+                    
+                    isService = pathname.indexOf(url.path) === 0;
+                    if (isService) {
+                        isStatic = false;
+                        return;
+                    }
+                });
+
+                if (!isService) {
+                    staticUrls.forEach((url: any) => {
+                        if (isStatic) { return; }
+
+                        isStatic = pathname.indexOf(url.path) === 0;
+                        if (isStatic) {
+                            isIndex = url.index;
+                            return;
+                        }
+                    });
                 }
-            });
+
+            }
         }
 
         if (isStatic) {
             let fileName: any;
             if (isIndex) {
                 fileName = path.resolve(path.join(process.cwd(), Config.APP_DEST, 'index.html'));
-                // fileName = path.resolve(path.join(process.cwd(), 'dist/dev/', 'index.html'));
             } else {
                 fileName = path.resolve(path.join(process.cwd(), Config.APP_DEST, pathname));
-                // fileName = path.resolve(path.join(process.cwd(), 'dist/dev/', pathname));
             }
-            // console.log('-- filename', fileName);
 
             fs.readFile(fileName, (err: any, data: any) => {
                 if (err) {
