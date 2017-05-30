db45417de4c10ccab485e9e808dcfeb25cfd8d9e
ysyun <ysyun@bistel.com>
2017-05-30 18:24:42 +0900

    feat(env): when refresh, check index.html based on index-uri.json because SPA

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -8,8 +8,7 @@ import { RouterModule } from '@angular/router';
                 /* define app module routes here, e.g., to lazily load a module
                 (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
                 */
-            ],
-            { useHash: true }
+            ]
         )
     ],
     exports: [RouterModule]
--- tools/server/middlewares/spa.ts
+++ tools/server/middlewares/spa.ts
@@ -8,6 +8,7 @@ import Config from '../../config';
 
 let staticUrls = require(path.join(process.cwd(), 'tools/config/stub_api/static-uri.json'));
 let serviceUrls = require(path.join(process.cwd(), 'tools/config/stub_api/service-uri.json'));
+let indexUrls = require(path.join(process.cwd(), 'tools/config/stub_api/index-uri.json'));
 
 module.exports = function (options: any) {
     if (!options) {
@@ -22,10 +23,33 @@ module.exports = function (options: any) {
         let isStatic: boolean = false;
         let isService: boolean = false;
         let isIndex: boolean = false;
-        if (pathname === '/') {
-            isStatic = true;
-            isIndex = true;
-        } else {
+        const paths = pathname.split('/');
+        let pathFirst: any = '';
+        if (paths.length === 1) {
+            pathFirst = '/';
+        } else if (paths.length > 1) {
+            pathFirst = '/' + paths[1];
+        }
+
+        /**
+         * If it is reload, set index.html because it is SPA.
+         * index.html을 불러올지 1차 판단
+         */
+        indexUrls.forEach((url: any) => {
+            if (isIndex) { return; }
+
+            isIndex = pathFirst === url.path;
+            if (isIndex) {
+                isStatic = true;
+                // util.log('>>>> index', pathname);
+                return;
+            }
+        });
+
+        /**
+         * service Restful 이면 proxy로 가고, static이면 static server에서 수행 
+         */
+        if (!isIndex) {
             if (pathname) {
                 // For Restful service call
                 serviceUrls.forEach((url: any) => {
