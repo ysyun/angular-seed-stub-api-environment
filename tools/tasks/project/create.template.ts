8929e5d60c7e47841f9ca9c5d063b0e64b66a911
ysyun <ysyun@bistel.com>
2017-08-11 17:52:21 +0900

    feat(template): add template for component/service

---
+++ tools/tasks/project/create.template.ts
@@ -0,0 +1,55 @@
+// import { createPlugin } from '../../utils';
+
+// import Config from '../../config';
+import * as gulp from 'gulp';
+import { join } from 'path';
+import { argv } from 'yargs';
+import * as util from 'gulp-util';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import Config from '../../config';
+
+const camelCase = require('camelcase');
+const capitalize = require('capitalize');
+const plugins = <any>gulpLoadPlugins();
+
+/**
+ * Not Used
+ */
+// export = createPlugin();
+
+export = (done: any) => {
+    let type = argv['type'] || 'component';
+    let name = argv['name'] || 'user';
+    if (type === 'c') {
+        type = 'component';
+    } else if (type === 's') {
+        type = 'service';
+    } else if (type === 'm') {
+        type = 'module';
+    }
+
+    let path = ''
+    if (name.indexOf('/') >= 0) {
+        const paths = name.split('/');
+        name = paths[paths.length - 1];
+        if (paths[0] !== '') {
+            path = paths.splice(0, paths.length - 1).join('/');
+        } else {
+            path = paths.splice(1, paths.length - 2).join('/');
+        }
+    }
+    Promise.resolve()
+        .then(() => {
+            gulp.src([
+                join(process.cwd(), 'tools/templates', type, '**', '*.ts'),
+                join(process.cwd(), 'tools/templates', type, '**', '*.scss'),
+                join(process.cwd(), 'tools/templates', type, '**', '*.html')
+            ])
+                .pipe(plugins.replace('#FILE_NAME#', name))
+                .pipe(plugins.replace('#NAME#', capitalize(camelCase(name))))
+                .pipe(plugins.rename({ prefix: name + '.' }))
+                .pipe(gulp.dest(join(Config.APP_SRC, Config.BOOTSTRAP_DIR, path)));
+
+            done();
+        });
+};
---
+++ tools/templates/component/component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'selector-name',
+    templateUrl: '#FILE_NAME#.component.html',
+    styleUrls: ['#FILE_NAME#.component.css']
+})
+export class #NAME#Component implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
